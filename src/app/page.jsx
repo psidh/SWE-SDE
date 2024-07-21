b1fd44bd2a4856660b8fa07dddbe0615aed085bb
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-07-21 15:34:45 +0530

    select option added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -66,97 +66,93 @@ export default function Page() {
   const groupedData = groupByTopic(data);
   return (
     <div>
-        <div className="py-24 px-4 md:px-16">
-          <Toaster />
-          <h1 className="text-2xl text-center flex-1 md:text-4xl py-6 px-4 font-medium">
-            Software (Developer) Engineer
-          </h1>
-          <div className="flex space-x-4 py-4 text-3xl w-full items-center justify-center">
-            <FaGoogle />
-            <FaApple />
-            <FaMeta />
-            <FaAmazon />
-            <FaMicrosoft />
-          </div>
-          {Object.entries(groupedData).map(([topic, questions]) => (
-            <div key={topic} className="py-6">
-              <h2 className="font-mono mb-8 text-xl py-2 px-4 rounded-lg mx-4 text-center md:text-2xl">
-                {topic === "DP" ? "Dynamic Programming" : topic}
-              </h2>
-              <div className="grid grid-cols-1 gap-x-8 gap-y-4">
-                {questions.map((question, index) => (
-                  <div
-                    key={question._id}
-                    className="flex items-center border border-neutral-800 rounded-xl p-4"
-                  >
-                    {editIndex === index ? (
-                      <>
-                        <input
-                          type="text"
-                          className="flex-1 mr-4 py-2 px-4 bg-neutral-900 text-white rounded"
-                          value={localEditData.name}
-                          onChange={(e) => handleChange("name", e.target.value)}
-                        />
-                        <input
-                          type="number"
-                          className="flex-1 mr-4 py-2 px-4 bg-neutral-900 text-white rounded"
-                          value={localEditData.times}
-                          onChange={(e) =>
-                            handleChange("times", e.target.value)
-                          }
-                        />
-                        <input
-                          type="text"
-                          className={`flex-1 py-2 px-4 bg-neutral-900 rounded ${
-                            localEditData.difficulty === "Easy"
-                              ? "text-neutral-100"
-                              : localEditData.difficulty === "Medium"
-                              ? "text-neutral-400"
-                              : "text-neutral-600"
-                          }`}
-                          value={localEditData.difficulty}
-                          onChange={(e) =>
-                            handleChange("difficulty", e.target.value)
-                          }
-                        />
-                        <button
-                          className="bg-white text-black py-2 px-4 rounded ml-4"
-                          onClick={handleUpdateClick}
-                        >
-                          Update
-                        </button>
-                      </>
-                    ) : (
-                      <>
-                        <p className="flex-1 mr-4 py-2 px-4">{question.name}</p>
-                        <p className="flex-1 mr-4 py-2 px-4">
-                          {question.times}
-                        </p>
-                        <p
-                          className={`flex-1 py-2 px-4 ${
-                            question.difficulty === "Easy"
-                              ? "text-neutral-50"
-                              : question.difficulty === "Medium"
-                              ? "text-neutral-400"
-                              : "text-neutral-600"
-                          }`}
-                        >
-                          {question.difficulty}
-                        </p>
-                        <button
-                          className="bg-neutral-800 text-white py-2 px-4 rounded"
-                          onClick={() => handleEditClick(index)}
-                        >
-                          Edit
-                        </button>
-                      </>
-                    )}
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+      <div className="py-24 px-4 md:px-16">
+        <Toaster />
+        <h1 className="text-2xl text-center flex-1 md:text-4xl py-6 px-4 font-medium">
+          Software (Developer) Engineer
+        </h1>
+        <div className="flex space-x-4 py-4 text-3xl w-full items-center justify-center">
+          <FaGoogle />
+          <FaApple />
+          <FaMeta />
+          <FaAmazon />
+          <FaMicrosoft />
         </div>
+        {Object.entries(groupedData).map(([topic, questions]) => (
+          <div key={topic} className="py-6">
+            <h2 className="font-mono mb-8 text-xl py-2 px-4 rounded-lg mx-4 text-center md:text-2xl">
+              {topic === "DP" ? "Dynamic Programming" : topic}
+            </h2>
+            <div className="grid grid-cols-1 lg:gap-x-8 gap-y-4">
+              {questions.map((question, index) => (
+                <div key={question._id} className="">
+                  {editIndex === index ? (
+                    <div className="flex lg:flex-row flex-col items-center justify-center border border-neutral-800 rounded-xl p-4">
+                      <input
+                        type="text"
+                        className="flex-1 lg:mr-4 py-2 px-4 bg-neutral-900 text-white rounded my-2"
+                        value={localEditData.name}
+                        onChange={(e) => handleChange("name", e.target.value)}
+                      />
+                      <input
+                        type="number"
+                        className="flex-1 lg:mr-4 py-2 px-4 bg-neutral-900 text-white rounded my-2"
+                        value={localEditData.times}
+                        onChange={(e) => handleChange("times", e.target.value)}
+                      />
+                      <select
+                        className={`flex-1 py-2 px-4 bg-neutral-900 rounded my-2 ${
+                          localEditData.difficulty === "Easy"
+                            ? "text-neutral-100"
+                            : localEditData.difficulty === "Medium"
+                            ? "text-neutral-400"
+                            : "text-neutral-600"
+                        }`}
+                        value={localEditData.difficulty}
+                        onChange={(e) =>
+                          handleChange("difficulty", e.target.value)
+                        }
+                      >
+                        <option value="Easy">Easy</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Hard">Hard</option>
+                      </select>
+                      <button
+                        className="bg-white text-black py-2 px-4 rounded lg:ml-4 my-2"
+                        onClick={handleUpdateClick}
+                      >
+                        Update
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="flex items-center border border-neutral-800 rounded-xl p-4">
+                      <p className="flex-1 mr-4 py-2 px-4">{question.name}</p>
+                      <p className="flex-1 mr-4 py-2 px-4">{question.times}</p>
+                      <p
+                        className={`flex-1 py-2 px-4 ${
+                          question.difficulty === "Easy"
+                            ? "text-neutral-50"
+                            : question.difficulty === "Medium"
+                            ? "text-neutral-400"
+                            : "text-neutral-600"
+                        }`}
+                      >
+                        {question.difficulty}
+                      </p>
+                      <button
+                        className="bg-neutral-800 text-white py-2 px-4 rounded"
+                        onClick={() => handleEditClick(index)}
+                      >
+                        Edit
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
