5d073b29016fd9c8a041cf093e29abc71a3d7d80
Manas Malla <manasmalla.dev@gmail.com>
2024-07-21 23:13:22 +0530

    testing for production
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,7 +21,8 @@ export default function Page() {
         },
       }, {cache: "no-store"});
       const data = await response.json();
-      // console.log(data);
+
+      console.log(data);
       setData(data);
     };
 
