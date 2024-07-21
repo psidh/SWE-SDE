39adac2f10430f3174341129b9d4d588607b07c9
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-07-21 20:40:06 +0530

    commit

--- src/app/api/v1/route.ts
+++ src/app/api/v1/route.ts
@@ -12,11 +12,16 @@ export async function GET() {
 export async function PUT(request: NextRequest) {
   try {
     const data = await request.json();
+    console.log(data);
+    
     const { _id, name, times, difficulty } = data[0];
+
     const question = await Question.findByIdAndUpdate(_id, { name, times, difficulty }, { new: true });
+    console.log(question);
     if (!question) {
       return NextResponse.json({ error: "Question not found" }, { status: 404 });
     }
+    
     return NextResponse.json(question);
   } catch (error) {
     console.error(error);
